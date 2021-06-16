db80ed3adddbb6c5c96aaff67f7651555267e933
Jimmy <jimmy.toor.s@gmail.com>
2021-06-15 19:43:18 -0700

    Added alternate movement implementation
    This will work better when the rig is rotated on X or Y which can happen while swimming

---
+++ Assets/Scripts/AltMove.cs
@@ -0,0 +1,50 @@
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    public class AltMove : ActionBasedContinuousMoveProvider
+    {
+        // Alternative method of calculating movement translation that works better with X and Z rotations, which we need for swimming
+        protected override Vector3 ComputeDesiredMove(Vector2 input)
+        {
+            if (input == Vector2.zero)
+                return Vector3.zero;
+
+            var xrRig = system.xrRig;
+            if (xrRig == null)
+                return Vector3.zero;
+
+            // Assumes that the input axes are in the range [-1, 1].
+            // Clamps the magnitude of the input direction to prevent faster speed when moving diagonally,
+            // while still allowing for analog input to move slower (which would be lost if simply normalizing).
+            var inputMove = Vector3.ClampMagnitude(new Vector3(enableStrafe ? input.x : 0f, 0f, input.y), 1f);
+
+            var rigTransform = xrRig.rig.transform;
+            var rigUp = rigTransform.up;
+
+            // Determine frame of reference for what the input direction is relative to
+            var forwardSourceTransform = forwardSource == null ? xrRig.cameraGameObject.transform : forwardSource;
+            var inputForwardInWorldSpace = forwardSourceTransform.forward;
+            var inputRightInWorldSpace = forwardSourceTransform.right;
+            
+            if (Mathf.Approximately(Mathf.Abs(Vector3.Dot(inputForwardInWorldSpace, rigUp)), 1f))
+            {
+                // When the input forward direction is parallel with the rig normal,
+                // it will probably feel better for the player to move along the same direction
+                // as if they tilted forward or up some rather than moving in the rig forward direction.
+                // It also will probably be a better experience to at least move in a direction
+                // rather than stopping if the head/controller is oriented such that it is perpendicular with the rig.
+                inputForwardInWorldSpace = -forwardSourceTransform.up;
+            }
+
+            float movementZ = forwardSourceTransform.InverseTransformDirection(inputForwardInWorldSpace).z * inputMove.z;
+            float movementX = forwardSourceTransform.InverseTransformDirection(inputRightInWorldSpace).x * inputMove.x;
+            var movement = Vector3.ProjectOnPlane(forwardSourceTransform.TransformDirection(new Vector3(movementX,0,movementZ)),rigUp);
+            var translationInWorldSpace = movement * (moveSpeed * Time.deltaTime); 
+
+            return translationInWorldSpace;
+        }
+    }
+}
